32fbf2c5867cd8473072c997a557014436fc3172
David Regla <dreglad@gmail.com>
2018-02-06 01:35:29 -0600

    adidng helper tasks

--- app/dvr/tasks/videos.py
+++ app/dvr/tasks/videos.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 from celery import shared_task, group
 from celery.result import allow_join_result
 import pexpect
+import youtube_dl
 
 from dvr.models import Video
 
@@ -65,3 +66,69 @@ def process_video(video_pk):
     else:
         video.set_status('FAILURE', result=vinfo or None)
 
+
+
+
+@shared_task
+def download_video_youtubedl(url, filename, video_pk=None):
+    if video_pk: video = Video.objects.get(pk=video_pk)
+
+    def youtubedl_progress(d):
+        if video_pk:
+            if d['status'] == 'downloading':
+                progress = d.get('fragment_index', 0)/float(d.get('fragment_count', 0))
+                video.set_status('STARTED', progress=progress - 0.03)
+            elif d['status'] == 'finished':
+                video.set_status('STARTED', progress=0.99)
+            elif d['status'] == 'error':
+                video.set_status('ERROR', result=d)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'logger': logger,
+        # 'hls_use_mpegts': True,
+        'hls_prefer_native': True,
+        'outtmpl': filename,
+        'progress_hooks': [youtubedl_progress],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            print('About to download: ', url)
+            ydl.download([url])
+            return True
+        except youtube_dl.utils.DownloadError:
+            video.set_status('FAILURE', result={ 'failed_download': url })
+            return False
+
+
+@shared_task
+def download_video_ffmpeg(url, filename, video_pk=None):
+    if video_pk: video = Video.objects.get(pk=video_pk)
+
+    if not os.path.isdir(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
+    cmd = 'ffmpeg -y -i "{}" -c copy {}'.format(url, filename)
+    print('Executing command: ', cmd)
+    thread = pexpect.spawn(cmd)
+    cpl = thread.compile_pattern_list([
+        pexpect.EOF,
+        'Duration: (\d\d:\d\d:\d\d\.?\d*)',
+        'time=(\d\d:\d\d:\d\d\.?\d*)'
+        ])
+    prev_progress, duration, progress = None, 0, 0
+    while True:
+        i = thread.expect_list(cpl, timeout=None)
+        if i == 0: # EOF
+            print('subprocess finished')
+            return
+        elif i == 1:
+            duration = thread.match.group(1)
+            print('duration', duration)
+        elif i == 2:
+            progress_time = thread.match.group(1)
+            print(progress_time)
+        if video_pk and duration and (progress != prev_progress):
+            pass
+            #video.set_status('STARTED', pregress=xxxxx)
+    thread.close()
