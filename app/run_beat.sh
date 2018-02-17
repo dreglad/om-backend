7428e2a1d4fafbc9e03d7ecb6fa9643a845eed0c
David Regla <dreglad@gmail.com>
2018-02-17 00:23:06 -0600

    Don't execute Djano's DB migrations on celery and beat services' compose command script

--- app/run_beat.sh
+++ app/run_beat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 5
-bash -c "python3 manage.py migrate && celery -A live2vod beat -l info"
+bash -c "celery -A live2vod beat -l info"
--- app/run_celery.sh
+++ app/run_celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash -c "python3 manage.py migrate && celery -A live2vod worker -Q celery -l info --concurrency 6 -E"
+bash -c "celery -A live2vod worker -Q celery -l info --concurrency 6 -E"
