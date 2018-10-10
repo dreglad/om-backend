6ceede4ead292cb751c46e34998eb0c1d5c7a4e5
David Regla <dreglad@gmail.com>
2018-10-10 00:59:26 -0500

    :zap: Iproving live video testing

--- app/live2vod/views.py
+++ app/live2vod/views.py
@@ -1,24 +1,24 @@
+from datetime import timedelta, datetime
 from posixpath import join
 from urllib.parse import urljoin
 
 from django.http import HttpResponse
+from django.urils import timezone
 
 from dvr.models import Stream
-from dvr.tasks.video_utils import download_live_video_sample, get_video_info
+from dvr.tasks.video_utils import download_live_video_sample, get_video_duration
 
 
 def check_streams(request):
     for stream in Stream.objects.all():
-        m = stream.metadata
-        filename = '/tmp/temp.mp4'
-        test_url = urljoin(m['wseStreamingUrl'],
-                           '{wseApplication}/{wseStream}/playlist.m3u8').format(**m)
-        download_live_video_sample(test_url, time=6, filename=filename)
-        info = get_video_info(filename)
-        if info.get('duration') > '5000':
-            print('Streaming OK')
-        else:
-            print('Streaming not OK, reseting')
+        try:
+            # Test stream metadata for a recent recording time
+            end = datetime.fromtimestamp(stream.get_provider().get_data()['current_store_details']['utcEnd']/1000.0)
+            assert end > timezone.now() - timedelta(minutes=3)
+            # Download and test a sample video
+            assert _test_live(stream)
+        except Exception as e:
+            print('Streaming not OK: %s' % e)
             reset_url = urljoin(m['wseApiUrl'], (
                 '/v2/servers/_defaultServer_/vhosts/_defaultVHost_'
                 '/applications/{wseApplication}/instances/_definst_'
@@ -26,3 +26,14 @@ def check_streams(request):
                 ).format(**m)
             )
     return HttpResponse('Ok')
+
+
+def _test_live(stream):
+    """Tests live HLS stream"""
+    filename = '/tmp/temp.mp4'
+    test_url = urljoin(stream.metadata['wseStreamingUrl'],
+                       '{wseApplication}/{wseStream}/playlist.m3u8').format(**stream.metadata)
+    download_live_video_sample(test_url, time=6, filename=filename)
+
+    print(info['duration'])
+    return get_video_duration(filename) > timedelta(seconds=4)
